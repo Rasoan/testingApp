ab7b11acb847e60fc45e178dfbd37533512536a3
Araik <araikrasoian@gmail.com>
2021-04-21 14:14:56 +0300

    laid out a modal window

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
 import style from "./ModalWindow.module.scss";
+import { autorun } from 'mobx'
 
 
 
 const ModalWindow = (props) => {
     const {closeModalWindow, name, surname} = props;
+    const upHandler = ({key}) => {
+      if(key !== "Escape") return;
+      closeModalWindow();
+    }
+
+    React.useEffect(() => {
+      autorun(() => {
+        window.addEventListener("keyup", upHandler);
+      });
+    },[])
+
     return (
-      <div>
-        <h1>Здравствуй</h1>
-        <p>{surname}</p>
-        <p>{name}</p>
-        <button onClick={closeModalWindow}>Закрыть окно</button>
+      <>
+      <div onClick={closeModalWindow} className={style.modalWrapper}></div>
+      <div className={style.modalContainer}>
+        <div className={style.itemContainerButtonClose}>
+          <button onClick={closeModalWindow} className={style.buttonClose}>X</button>
+        </div>
+        <h1 className={style.modalHeader}>Hello world!</h1>
+        <div className={style.itemContainer}>
+          <p className={style.item}>{surname}</p>
+        </div>
+        <div className={style.itemContainer}>
+          <p className={style.item}>{name}</p>
+        </div>
+          <button onClick={closeModalWindow} className={style.okModalWindow}>Ok</button>
       </div>
+      </>
     )
 }
 
